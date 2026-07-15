dd697672836985d5d249803661d68a9276d14bc0
Mariakevin <mariakevin0504@gmail.com>
2026-07-15 20:46:15 +0530

    fix: correct timm model names for local version
    - swin: remove .ms_in22k suffix
    - maxvit: maxvit_base_224.sw_in1k -> maxvit_base_tf_224
    - convnext_v2: remove .fcmae_ft_in1k suffix
    - efficientnet_v2: efficientnet_v2_m -> efficientnet_b3 (v2 not available)
    - deit: remove .fb_in22k_ft_in1k suffix

--- retina_app/constants.py
+++ retina_app/constants.py
@@ -28,11 +28,11 @@ MODEL_WEIGHTS = {
 
 # Mapping from short names to timm model identifiers
 MODEL_NAME_MAP = {
-    "swin": "swin_tiny_patch4_window7_224.ms_in22k",
-    "maxvit": "maxvit_base_224.sw_in1k",
-    "convnext_v2": "convnextv2_base.fcmae_ft_in1k",
-    "efficientnet_v2": "efficientnet_v2_m.orig_in21k_ft_in1k",
-    "deit": "deit3_base_patch16_224.fb_in22k_ft_in1k",
+    "swin": "swin_tiny_patch4_window7_224",
+    "maxvit": "maxvit_base_tf_224",
+    "convnext_v2": "convnextv2_base",
+    "efficientnet_v2": "efficientnet_b3",
+    "deit": "deit3_base_patch16_224",
 }
 
 CLASS_PERFORMANCE_WEIGHTS = {
